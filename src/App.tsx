6d10956acdca3a82b4f5aef94b95f23327df676f
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-07-16 18:34:30 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styles from "./app.module.css";
 function App() {
+  const [wordList, setWordList] = useState<string[]>([
+    "     ",
+    "     ",
+    "     ",
+    "     ",
+    "     ",
+    "     ",
+  ]);
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      tabIndex={0}
+      className={styles.root}
+      onKeyDown={(e) => {
+        const currentWord = wordList[count];
+        let curr = [...wordList];
+
+        let mod = currentWord.split("");
+        mod[currentWord.replace(/\s/g, "").length] = e.key;
+
+        if (mod.join("").replace(/\s/g, "").length < 6) {
+          if (e.key !== "Enter") {
+            curr[count] = mod.join("");
+          }
+        }
+
+        setWordList(curr);
+
+        if (e.key === "Enter" && curr[count].length === 5) {
+          setCount(count + 1);
+        }
+      }}
+    >
+      <div className={styles.wordList}>
+        {wordList.map((word) => (
+          <div className={styles.wordItem}>
+            {word.split("").map((letter) => (
+              <div className={styles.letter}>{letter}</div>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
