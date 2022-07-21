03a29179dbfc8df9434fcf56155f9242d0862840
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-07-21 00:02:47 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
+// import * as React from "react";
+// import Stack from "@mui/material/Stack";
 import logo from "./logo.svg";
 import "./App.css";
 import styles from "./app.module.css";
+import axios from "axios";
+import { removeWhiteSpaces } from "./helper";
+import classNames from "classnames";
+// import Button from "@mui/material/Button";
+
 function App() {
   const [wordList, setWordList] = useState<string[]>([
     "     ",
@@ -12,7 +19,11 @@ function App() {
     "     ",
   ]);
 
+  const [wordColors, setWordColors] = useState<string[][]>([[]]);
+
   const [count, setCount] = useState(0);
+
+  const wordGuess = "enter";
 
   return (
     <div
@@ -22,10 +33,13 @@ function App() {
         const currentWord = wordList[count];
         let curr = [...wordList];
 
+        // split currentWord into array of characters
         let mod = currentWord.split("");
-        mod[currentWord.replace(/\s/g, "").length] = e.key;
+        // replace current char with corresponding index of mod
+        mod[removeWhiteSpaces(currentWord).length] = e.key;
 
-        if (mod.join("").replace(/\s/g, "").length < 6) {
+        // if mod length is less than 6 join mod
+        if (removeWhiteSpaces(mod.join("")).length < 6) {
           if (e.key !== "Enter") {
             curr[count] = mod.join("");
           }
@@ -34,16 +48,59 @@ function App() {
         setWordList(curr);
 
         if (e.key === "Enter" && curr[count].length === 5) {
-          setCount(count + 1);
+          axios
+            .get(
+              `https://api.dictionaryapi.dev/api/v2/entries/en/${curr[count]}`
+            )
+            .then(function (response: any) {
+              // handle success
+              const word = response.data[0].word;
+              if (!word) {
+                console.log(response.data.title);
+              } else {
+                console.log(word);
+                let color = [];
+                for (let i = 0; i < 5; i++) {
+                  const currWordChar = curr[count].charAt(i);
+                  if (currWordChar === wordGuess.charAt(i)) {
+                    color[i] = styles.green;
+                  } else if (wordGuess.includes(currWordChar)) {
+                    color[i] = styles.yellow;
+                  } else {
+                    color[i] = styles.gray;
+                  }
+                }
+                setCount(count + 1);
+
+                let newColor = wordColors;
+                newColor[count] = color;
+                setWordColors(newColor);
+              }
+            });
         }
       }}
     >
       <div className={styles.wordList}>
-        {wordList.map((word) => (
+        {wordList.map((word, wordIndex) => (
           <div className={styles.wordItem}>
-            {word.split("").map((letter) => (
-              <div className={styles.letter}>{letter}</div>
-            ))}
+            {word.split("").map((letter, index) => {
+              if (wordColors[wordIndex]?.[index]) {
+                return (
+                  <div
+                    className={classNames(
+                      styles.letter,
+                      wordColors[wordIndex][index]
+                    )}
+                  >
+                    {letter}
+                  </div>
+                );
+              } else {
+                return (
+                  <div className={classNames(styles.letter)}>{letter}</div>
+                );
+              }
+            })}
           </div>
         ))}
       </div>
