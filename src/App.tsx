82938df5a120b592ae0a14cd5d654f7a6bb80e4f
hwajingh <51863703+hwajingh@users.noreply.github.com>
2022-07-22 15:46:03 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
 
   const [count, setCount] = useState(0);
 
-  const wordGuess = "enter";
+  const wordGuess = "tiger";
 
   return (
     <div
@@ -36,8 +36,11 @@ function App() {
         // split currentWord into array of characters
         let mod = currentWord.split("");
         // replace current char with corresponding index of mod
-        mod[removeWhiteSpaces(currentWord).length] = e.key;
-
+        if (e.key === "Backspace") {
+          mod[removeWhiteSpaces(currentWord).length - 1] = " ";
+        } else {
+          mod[removeWhiteSpaces(currentWord).length] = e.key;
+        }
         // if mod length is less than 6 join mod
         if (removeWhiteSpaces(mod.join("")).length < 6) {
           if (e.key !== "Enter") {
@@ -56,7 +59,7 @@ function App() {
               // handle success
               const word = response.data[0].word;
               if (!word) {
-                console.log(response.data.title);
+                alert(response.data.title);
               } else {
                 console.log(word);
                 let color = [];
@@ -103,6 +106,106 @@ function App() {
             })}
           </div>
         ))}
+
+        <div className="Keyboard-module_row__YWe5w">
+          <button type="button" data-key="q" className="Key-module_key__Rv-Vp">
+            q
+          </button>
+          <button type="button" data-key="w" className="Key-module_key__Rv-Vp">
+            w
+          </button>
+          <button type="button" data-key="e" className="Key-module_key__Rv-Vp">
+            e
+          </button>
+          <button type="button" data-key="r" className="Key-module_key__Rv-Vp">
+            r
+          </button>
+          <button type="button" data-key="t" className="Key-module_key__Rv-Vp">
+            t
+          </button>
+          <button type="button" data-key="y" className="Key-module_key__Rv-Vp">
+            y
+          </button>
+          <button type="button" data-key="u" className="Key-module_key__Rv-Vp">
+            u
+          </button>
+          <button type="button" data-key="i" className="Key-module_key__Rv-Vp">
+            i
+          </button>
+          <button type="button" data-key="o" className="Key-module_key__Rv-Vp">
+            o
+          </button>
+          <button type="button" data-key="p" className="Key-module_key__Rv-Vp">
+            p
+          </button>
+
+          <div className="Keyboard-module_row__YWe5w">
+            <div data-testid="spacer" className="Key-module_half__ljsj8"></div>
+            <button
+              type="button"
+              data-key="a"
+              className="Key-module_key__Rv-Vp"
+            >
+              a
+            </button>
+            <button
+              type="button"
+              data-key="s"
+              className="Key-module_key__Rv-Vp"
+            >
+              s
+            </button>
+            <button
+              type="button"
+              data-key="d"
+              className="Key-module_key__Rv-Vp"
+            >
+              d
+            </button>
+            <button
+              type="button"
+              data-key="f"
+              className="Key-module_key__Rv-Vp"
+            >
+              f
+            </button>
+            <button
+              type="button"
+              data-key="g"
+              className="Key-module_key__Rv-Vp"
+            >
+              g
+            </button>
+            <button
+              type="button"
+              data-key="h"
+              className="Key-module_key__Rv-Vp"
+            >
+              h
+            </button>
+            <button
+              type="button"
+              data-key="j"
+              className="Key-module_key__Rv-Vp"
+            >
+              j
+            </button>
+            <button
+              type="button"
+              data-key="k"
+              className="Key-module_key__Rv-Vp"
+            >
+              k
+            </button>
+            <button
+              type="button"
+              data-key="l"
+              className="Key-module_key__Rv-Vp"
+            >
+              l
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
